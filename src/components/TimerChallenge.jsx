75abd5782a431090dd62caadf3545754600e3400
Dimitar <dimitar.mandarliev@gmail.com>
2025-03-13 08:51:22 +0200

    Refactor TimerChallenge component to include stop functionality and optimize timer management; remove unnecessary div in Player component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
   }
 
-  function handleStop() {}
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
@@ -20,7 +23,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? "s" : ``}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
