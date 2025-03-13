9acee344a4c3df8c57c018563f7fbc0cb3a57f5c
Dimitar <dimitar.mandarliev@gmail.com>
2025-03-13 07:27:38 +0200

    Add TimerChallenge component and integrate it into App; update Player component layout

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,6 +11,7 @@ export default function Player() {
     <section id="player">
       <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
+        <div></div>
         <input ref={playerNameRef} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
